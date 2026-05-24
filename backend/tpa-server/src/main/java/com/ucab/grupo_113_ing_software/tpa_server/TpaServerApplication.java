8c67505a37ca2b7c70683100bccc970d683deda8
Luis Maduro <luisr@iconos.com.ve>
2026-05-24 19:52:30 -0400

    initial commit

---
+++ backend/tpa-server/src/main/java/com/ucab/grupo_113_ing_software/tpa_server/TpaServerApplication.java
@@ -0,0 +1,13 @@
+package com.ucab.grupo_113_ing_software.tpa_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpaServerApplication.class, args);
+	}
+
+}
