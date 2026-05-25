f0768eb54e22a40d6f4f60b7a283f5d80446d695
Luis Maduro <luisr@iconos.com.ve>
2026-05-25 00:20:40 -0400

    api funcional con usuario con requests post y get

--- backend/tpa-server/src/main/java/com/ucab/grupo_113_ing_software/tpa_server/model/Usuario.java
+++ backend/tpa-server/src/main/java/com/ucab/grupo_113_ing_software/tpa_server/model/Usuario.java
@@ -18,6 +18,46 @@ public class Usuario {
     @Enumerated(EnumType.STRING) // 👈 Le dice a H2 que guarde el rol como texto y no como un número
     private Rol rol;
 
-    // (Aquí luego generaremos los getters, setters y constructores)
+    public Usuario() {
+    }
+
+    public Usuario(Long id, String cedula, String clave, Rol rol) {
+        this.id = id;
+        this.cedula = cedula;
+        this.clave = clave;
+        this.rol = rol;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    public Rol getRol() {
+        return rol;
+    }
+
+    public void setRol(Rol rol) {
+        this.rol = rol;
+    }
 }
 
