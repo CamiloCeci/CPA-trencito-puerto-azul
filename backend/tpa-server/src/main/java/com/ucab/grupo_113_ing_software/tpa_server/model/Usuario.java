d5d9506e446f618f829e869345803d38b7956570
carlos <calaverde.25@est.ucab.ve>
2026-05-24 23:05:45 -0400

    commit 2

--- backend/tpa-server/src/main/java/com/ucab/grupo_113_ing_software/tpa_server/model/Usuario.java
+++ backend/tpa-server/src/main/java/com/ucab/grupo_113_ing_software/tpa_server/model/Usuario.java
@@ -1,5 +1,23 @@
 package com.ucab.grupo_113_ing_software.tpa_server.model;
 
-public class Usuario {
+import jakarta.persistence.*;
 
+@Entity
+public class Usuario {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false) // 👈 Evita que dos personas se registren con la misma cédula
+    private String cedula;
+
+    @Column(nullable = false)
+    private String clave;
+
+    @Enumerated(EnumType.STRING) // 👈 Le dice a H2 que guarde el rol como texto y no como un número
+    private Rol rol;
+
+    // (Aquí luego generaremos los getters, setters y constructores)
 }
+
