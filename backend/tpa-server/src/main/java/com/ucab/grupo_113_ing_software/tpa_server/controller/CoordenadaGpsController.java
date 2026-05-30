1342aed1e3d5d72121adb8e9d3c36253e1e30e01
Luis Maduro <luisr@iconos.com.ve>
2026-05-30 10:52:18 -0400

    Fix GPS Service Controller Arrechisimo

--- backend/tpa-server/src/main/java/com/ucab/grupo_113_ing_software/tpa_server/controller/CoordenadaGpsController.java
+++ backend/tpa-server/src/main/java/com/ucab/grupo_113_ing_software/tpa_server/controller/CoordenadaGpsController.java
@@ -22,9 +22,13 @@ public class CoordenadaGpsController {
 
     @GetMapping("/")
     public ResponseEntity<CoordenadaGps> getCoordenadaGps() {
-        return gpsCoordenadasRepository.findById((long) 1).map(coord -> ResponseEntity.ok().body(coord))
-                .orElse(ResponseEntity.notFound().build());
+        CoordenadaGps coord = gpsService.getCoordenadas();
+        if  (coord != null)
+            return ResponseEntity.ok().body(coord);
+        else
+            return ResponseEntity.notFound().build();
     }
+
 
     @GetMapping("/{latitude}/{longitude}/")
     public ResponseEntity<?> updateCoordenadaGps(@PathVariable double latitude, @PathVariable double longitude) {
